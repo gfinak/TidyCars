da31a80dc7164665c14da79b2e2d27db8342d8b9
gfinak <greg.finak@gmail.com>
2018-09-07 14:45:50 -0700

    initial commit

---
+++ data-raw/documentation.R
@@ -0,0 +1,27 @@
+#' TidyCars
+#' A data package for TidyCars.
+#' @docType package
+#' @aliases TidyCars-package
+#' @title A tidied cars data set.
+#' @name TidyCars
+#' @description Cars but better. The variable names are more meaninful.
+#' @details The columns have been renamed to indicate the units and better describe what is measured.
+#' @seealso
+#' \link{tidy_cars}
+NULL
+
+
+
+#' The stopping distances of cars at different speeds.
+#' @name tidy_cars
+#' @docType data
+#' @title The stopping distances of cars traveling at different speeds.
+#' @format a \code{data.frame} containing the following fields:
+#' \describe{
+#' \item{speed_mph}{The speed of the vehicle.}
+#' \item{stopping_distace}{The stopping distance of the vehicle.}
+#' }
+#' @source The data comes from the cars data set distributed with R.
+#' @seealso
+#' \link{TidyCars}
+NULL
